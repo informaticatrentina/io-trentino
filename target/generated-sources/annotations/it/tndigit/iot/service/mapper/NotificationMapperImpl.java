c4a2ec97c3f8445ce452271f25c8fff2797f4d24
mirkopianetti <mirko.pianetti@infotn.it>
2019-10-17 11:44:42 +0200

    Cancellazione cartella target

--- target/generated-sources/annotations/it/tndigit/iot/service/mapper/NotificationMapperImpl.java
+++ target/generated-sources/annotations/it/tndigit/iot/service/mapper/NotificationMapperImpl.java
@@ -13,7 +13,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2019-10-17T11:08:41+0200",
+    date = "2019-10-17T11:16:33+0200",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 1.8.0_222 (Private Build)"
 )
 @Component
